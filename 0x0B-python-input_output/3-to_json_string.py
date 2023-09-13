cf2d220d5c0138c8328c3db14790e242f9b378a5
titobzzz <olumoyinjoshua19609@gmail.com>
2023-09-12 19:20:00 -0700

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a string-to-JSON function"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    this returns the JSON representation of a string object"""
+    return json.dumps(my_obj)
