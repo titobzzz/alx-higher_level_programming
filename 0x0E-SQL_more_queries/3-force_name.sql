c1d0fc60b8f0aa7274906fb1abb564018b00c0b5
Titobi <olumoyinjoshua@gmail.com>
2023-12-13 18:10:52 -0800

    delted files

--- 0x0E-SQL_more_queries/3-force_name.sql
+++
@@ -1,5 +0,0 @@
--- creates the table force_name on your MySQL server.
--- force_name description: id INT, name VARCHAR(256) Not Null
--- The database name will be passed as an argument of the mysql comman
-
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
